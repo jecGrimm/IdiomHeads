09a24e8b4751e5fa5a66f6f7c722a6f354f21e19
Jana <janagrimm22@gmail.com>
2025-04-08 15:49:59 +0200

    FIXED path name

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@
 #SBATCH --comment="Test the idiom scorer on a GPU"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=j.grimm@campus.lmu.de
-#SBATCH --chdir=/home/IdiomHeads
-#SBATCH --output=/home/IdiomHeads/slurm.%j.%N.out
+#SBATCH --chdir=/home/g/grimmj/IdiomHeads
+#SBATCH --output=/home/g/grimmj/IdiomHeads/slurm.%j.%N.out
 #SBATCH --ntasks=1
 
 python3 -u small_detect_idiom.py
