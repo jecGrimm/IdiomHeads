cb4666caa855fdcdac803d7775ae26a76c070cb0
Jana <janagrimm22@gmail.com>
2025-04-08 14:49:00 +0200

    ADDED sanity check

--- small_detect_idiom.py
+++ small_detect_idiom.py
@@ -9,9 +9,12 @@ import torch as t
 model: HookedTransformer = HookedTransformer.from_pretrained("EleutherAI/pythia-14m")
 epie = EPIE_Data()
 scorer = Scorer(model)
+print("Running on device {scorer.device}.")
 
 formal_data = epie.create_hf_dataset(epie.formal_sents[:3], epie.tokenized_formal_sents[:3], epie.tags_formal[:3])
 formal_scores = scorer.create_data_score_tensor(formal_data)
+
+scorer.explore_tensor(formal_scores)
 
 if not os.path.isdir("./scores"):
     os.mkdir("./scores")
