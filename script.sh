a8db3c489cc296d3cac279e2cf66242c175f6305
Jana <janagrimm22@gmail.com>
2025-04-08 15:46:00 +0200

    ADDED script for CIP GPU

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --job-name=small_detect_idiom_gpu
+#SBATCH --comment="Test the idiom scorer on a GPU"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=j.grimm@campus.lmu.de
+#SBATCH --chdir=/home/IdiomHeads
+#SBATCH --output=/home/IdiomHeads/slurm.%j.%N.out
+#SBATCH --ntasks=1
+
+python3 -u small_detect_idiom.py
