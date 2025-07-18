0398c8b6ac67522447c8968987226384ef8a0906
Jana Grimm <grimmj@diamant.cip.ifi.lmu.de>
2025-07-18 11:17:52 +0200

    ADDED negative ablation scores pythia-1.4b

--- gpu_scripts/ablation.sh
+++ gpu_scripts/ablation.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=j.grimm@campus.lmu.de
 #SBATCH --chdir=/home/g/grimmj/IdiomHeads
-#SBATCH --output=/home/g/grimmj/IdiomHeads/scripts/output/slurm.%j.%N.out
+#SBATCH --output=/home/g/grimmj/IdiomHeads/gpu_scripts/output/slurm.%j.%N.out
 #SBATCH --ntasks=1
 
 python3 -u compute_ablation.py -d formal trans -s 0 0 -e None None -m "EleutherAI/Pythia-1.4B" -i "pythia_formal_idiom_pos_ablation.json" -a "pythia-1.4b_formal_DLA"
